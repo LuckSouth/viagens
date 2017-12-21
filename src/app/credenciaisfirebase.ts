71bcc7c4d47a4ae5e6d881539b84a2928a39a7c0
fernando-sma <fernandosma282@gmail.com>
2017-12-21 11:30:20 -0300

    v 1.3.3

--- src/app/credenciaisfirebase.ts
+++ src/app/credenciaisfirebase.ts
@@ -1,4 +1,5 @@
 export const firebaseConfig = {
+    //Do Henrique
 
     apiKey: "AIzaSyAeEH0pyA3MF5bF_MFaLAXSICeKLVWgYYo",
     authDomain: "app-tijuca-1f135.firebaseapp.com",
@@ -6,5 +7,15 @@ export const firebaseConfig = {
     projectId: "app-tijuca-1f135",
     storageBucket: "app-tijuca-1f135.appspot.com",
     messagingSenderId: "867905888320"
+
+    //Do Fernando
+
+    // apiKey: "AIzaSyBCZebcTYtzdHJ1TVcJIjE-P40RdnSeamI",
+    // authDomain: "app-tijuca-d482b.firebaseapp.com",
+    // databaseURL: "https://app-tijuca-d482b.firebaseio.com",
+    // projectId: "app-tijuca-d482b",
+    // storageBucket: "app-tijuca-d482b.appspot.com",
+    // messagingSenderId: "1092667816937"
+
 
 }
