8d057d5bbf5c42aa4aeb01a06fa0804f6deab84e
luckperry <lucas.norte@tijucaalimentos.com>
2018-01-03 08:06:49 -0300

    v 1.3.9 b

--- src/pages/modulo-viagens/relatorios/relatorios.ts
+++ src/pages/modulo-viagens/relatorios/relatorios.ts
@@ -9,6 +9,7 @@ import { StorageProvider } from '../../../providers/storage/storage'
 
 interface IStorage{
   despesa:string;
+  data: string;
   valor:string;
 }
 
@@ -19,8 +20,10 @@ interface IStorage{
 })
 export class RelatoriosPage {
 
-  storage:IStorage = {despesa:'',valor:''};
+  storage:IStorage = {despesa:'',data:'',valor:''};
   storages:IStorage[];
+  editando: boolean = false;
+  storageEditando: IStorage;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -37,9 +40,35 @@ export class RelatoriosPage {
   adicionar(){
     if(this.storage.despesa != "" && this.storage.valor != ""){
     this.storageProvider.adicionar(this.storage);  
-    this.storage = {despesa:'',valor:''}    
+    this.storage = {despesa:'',data:'',valor:''}    
     }
 
+  }
+
+  // Editar o Storage
+  editar(storage: IStorage){
+    this.storage = {despesa:storage.despesa,data:storage.data,valor:storage.valor};
+    this.editando = true;
+    this.storageEditando = storage;
+  }
+
+  // Cancelar a edição
+  cancelarEditar(){
+    this.storage = {despesa:'',data:'',valor:''};
+    this.editando = false;
+  }
+
+  // Atualizar Storage
+  atualizar(){
+    if(this.storage.despesa != "" && this.storage.valor != ""){
+      this.storageProvider.atualizar(this.storageEditando,this.storage);  
+      this.cancelarEditar();
+    }  
+  }
+
+  // Deletar Storage
+  deletar(storage: IStorage){
+    this.storageProvider.deletar(storage);
   }
 
 }
